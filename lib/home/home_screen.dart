7412fb37e86ee4d84bfa6fd775a37b3f8f7a55ca
kajal.tiwari <kajal.tiwari@atriina.com>
2023-05-03 14:49:28 +0530

    CR:Kajal bottom navigation bar

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/home/following/followings_video_screen.dart';
+import 'package:tiktok_clone/home/for_you/for_you_video_screen.dart';
+import 'package:tiktok_clone/home/profile/profile_screen.dart';
+import 'package:tiktok_clone/home/search/search_screen.dart';
+import 'package:tiktok_clone/home/upload_video/upload_custom_icon.dart';
+import 'package:tiktok_clone/home/upload_video/upload_video_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,12 +14,56 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int screenIndex = 0;
+  List screensList =
+  [
+    ForYouVideoScreen(),
+    SearchScreen(),
+    UploadVideoScreen(),
+    FollowingsVideoScreen(),
+    Profile()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Dashboard Screen"),
-      )
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index)
+        {
+          setState(() {
+            screenIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white12,
+        currentIndex: screenIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30,),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 30,),
+            label: "Discover",
+          ),
+          BottomNavigationBarItem(
+            icon: UploadCustomIcon(),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inbox_sharp, size: 30,),
+            label: "Following",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30,),
+            label: "Me",
+          )
+        ],
+      ),
+      body: screensList[screenIndex],
     );
   }
 }
