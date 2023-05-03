9e2ad016f764d82771a151db5c84ba09cb34e2dd
kajal.tiwari <kajal.tiwari@atriina.com>
2023-05-03 16:05:01 +0530

    CR:Kajal upload button

--- lib/home/upload_video/upload_video_screen.dart
+++ lib/home/upload_video/upload_video_screen.dart
@@ -10,9 +10,32 @@ class UploadVideoScreen extends StatefulWidget {
 class _UploadVideoScreenState extends State<UploadVideoScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("This is the Upload Video Screen", style: TextStyle(color: Colors.white),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30,),
+            const Icon(Icons.upload, size: 200,),
+            const SizedBox(height: 20,),
+            ElevatedButton(
+                onPressed: (){
+
+                },
+                style: ElevatedButton.styleFrom(
+                  // backgroudColor: Colors.green
+                ),
+                child: const Text(
+                  "Upload new video",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color:Colors.white
+                  ),
+                ))
+
+          ],
+        )
       ),
     );
   }
